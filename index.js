52454f698503ce4ef3158d174221f4525afb3891
Shaun Kaasten <shaunk@freshbooks.com>
2016-01-05 15:14:10 -0500

    Add pendo snippit and track page transitions.

--- index.js
+++ index.js
@@ -2,5 +2,28 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-pendo'
+  name: 'ember-cli-pendo',
+  isDevelopingAddon: function() {
+    return true;
+  },
+  contentFor: function(type, config) {
+    if (type === 'body') {
+      return `<script>
+        window.pendo_options = {
+          apiKey: '${config['ember-cli-pendo'].apiKey}',
+
+          // This is required to be able to load data client side
+          usePendoAgentAPI: true
+        };
+      (function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = ('https:' === document.location.protocol ? 'https://' : 'http://' ) + 'd3accju1t3mngt.cloudfront.net/js/pa.min.js';
+        var firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(script, firstScript);
+      })();
+          </script>`;
+    }
+  }
 };
