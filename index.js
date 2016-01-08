7167e5abd2e0b4b273f62b529f7023697bb9f373
Shaun Kaasten <shaunk@freshbooks.com>
2016-01-08 17:14:27 -0500

    Don't use es6 strings and don't include in test config.

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ module.exports = {
     return false;
   },
   contentFor: function(type, config) {
-    if (type === 'body') {
-      return `<script>
-        window.pendo_options = {
-          apiKey: '${config['ember-cli-pendo'].apiKey}',
-
-          // This is required to be able to load data client side
-          usePendoAgentAPI: true
-        };
-      (function() {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = ('https:' === document.location.protocol ? 'https://' : 'http://' ) + 'd3accju1t3mngt.cloudfront.net/js/pa.min.js';
-        var firstScript = document.getElementsByTagName('script')[0];
-        firstScript.parentNode.insertBefore(script, firstScript);
-      })();
-          </script>`;
+    if (type === 'body' && config !== 'test') {
+      return "<script>" +
+        "window.pendo_options = {" +
+        "  apiKey: '${config['ember-cli-pendo'].apiKey}'," +
+        "  // This is required to be able to load data client side" +
+        "  usePendoAgentAPI: true" +
+        "};" +
+        "(function() {" +
+        "  var script = document.createElement('script');" +
+        "  script.type = 'text/javascript';" +
+        "  script.async = true;" +
+        "  script.src = ('https:' === document.location.protocol ? 'https://' : 'http://' ) + 'd3accju1t3mngt.cloudfront.net/js/pa.min.js';" +
+        "  var firstScript = document.getElementsByTagName('script')[0];" +
+        "  firstScript.parentNode.insertBefore(script, firstScript);" +
+        "})();" +
+        "</script>`;";
     }
   }
 };
