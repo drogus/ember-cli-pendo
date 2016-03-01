2e43e451625f1db407e754d11ea486f52c6f54df
Piotr Sarnacki <drogus@gmail.com>
2016-03-01 12:16:59 +0100

    Setup Pendo in application initializer
    That's needed for applications that inject config after the app is
    built.

--- index.js
+++ index.js
@@ -5,26 +5,5 @@ module.exports = {
   name: 'ember-cli-pendo',
   isDevelopingAddon: function() {
     return false;
-  },
-  contentFor: function(type, config) {
-    var apiKey = config['ember-cli-pendo'] ? config['ember-cli-pendo'].apiKey : null;
-
-    if (type === 'head' && config.environment !== 'test' && apiKey) {
-      return "<script>\n" +
-        "window.pendo_options = {\n" +
-        "  apiKey: '" + apiKey + "',\n" +
-        "  // This is required to be able to load data client side\n" +
-        "  usePendoAgentAPI: true\n" +
-        "};\n" +
-        "(function() {\n" +
-        "  var script = document.createElement('script');\n" +
-        "  script.type = 'text/javascript';\n" +
-        "  script.async = true;\n" +
-        "  script.src = ('https:' === document.location.protocol ? 'https://' : 'http://' ) + 'd3accju1t3mngt.cloudfront.net/js/pa.min.js';\n" +
-        "  var firstScript = document.getElementsByTagName('script')[0];\n" +
-        "  firstScript.parentNode.insertBefore(script, firstScript);\n" +
-        "})();\n" +
-        "</script>\n";
-    }
   }
 };
